a462b93f8b22053a2fbfd451afebfbcc91486e0c
Aman Rana <72211447+Amanrana2002@users.noreply.github.com>
2023-06-04 15:28:37 +0530

    Time: 1 ms (100%), Space: 39.6 MB (86.47%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int addDigits(int num) {
+        // if( num % 9 == 0 && num != 0) return 9;
+        // if( num > 9) return num % 9;
+        // return num;
+
+        if(num == 0) return 0;
+        return num%9 == 0 ? 9 : num%9;
+    }
+}
