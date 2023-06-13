b9abfbd36501fe948b6ae11080de5d08dadbc39a
Aman Rana <72211447+Amanrana2002@users.noreply.github.com>
2023-06-13 10:16:55 +0530

    Time: 12 ms (5.07%), Space: 44 MB (5.21%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,16 +1,26 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Map<Integer, Integer> map = new HashMap<>(); // map for next greater element
-        Stack<Integer> stack = new Stack<>();
-        for (int num : nums2) {
-            while (!stack.empty() && stack.peek() < num) { // Pop elements from stack and update map with next greater element
-                map.put(stack.pop(), num);
+        Stack<Integer> st = new Stack<>();
+        int[] help = new int[nums2.length];
+        int[] ans = new int[nums1.length];
+        for(int i=nums2.length - 1; i>=0 ; i-- ){
+            while(st.size()>0 && nums2[i]>st.peek()){
+                st.pop();
             }
-            stack.push(num); // Push current element onto stack
+            if(st.size()<=0){
+                help[i]=-1;
+            }else{
+                help[i]=st.peek();
+            }
+            st.push(nums2[i]);
         }
-        for (int i = 0; i < nums1.length; i++) { // Check if each element in nums1 has a next greater element in map
-            nums1[i] = map.containsKey(nums1[i]) ? map.get(nums1[i]) : -1; // Update element in nums1 with next greater element or -1
+        for(int i=0;i<ans.length;i++){
+            for(int j=0;j<nums2.length;j++){
+                if(nums1[i]==nums2[j]){
+                    ans[i]=help[j];
+                }
+            }
         }
-        return nums1;
+        return ans;
     }
 }
